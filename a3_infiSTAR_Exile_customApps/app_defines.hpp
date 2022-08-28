ac9ce1775b350eec4868d0a8860c9cf9c0e1d3d5
Desa2k <109931246+Desa2k@users.noreply.github.com>
2022-08-27 23:49:30 -0400

    Add files via upload

---
+++ a3_infiSTAR_Exile_customApps/app_defines.hpp
@@ -0,0 +1,8 @@
+class customapp_2 {
+	submenu = 0;
+	toggleable = 0;
+	text = "Recruit Ai Soldier";
+	tooltip = "Recruit Ai Soldier";
+	fnc = "closeDialog 1; createDialog ""d2kRecruitAi"";";
+	pic = "\a3\Ui_f\data\IGUI\Cfg\MPTable\infantry_ca.paa";
+};
